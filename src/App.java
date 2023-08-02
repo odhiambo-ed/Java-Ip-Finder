cb56ea6b360c9d2414e370a027d601e9e03c031a
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-02 15:22:12 +0300

    Add JFrame

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame jFrame = new JFrame("Ip Finder App");
+        jFrame.setBounds(100, 100, 800, 200);
+        jFrame.setVisible(true);
+
+        JLabel label = new JLabel("Enter URL");
+        label.setBounds(10, 10, 50, 20);
+        label.setVisible(true);
+
+        JTextField textField = new JTextField();
+        textField.setBounds(10, 10, 50, 20);
+        textField.setVisible(true);
+
+        jFrame.add(label);
+        jFrame.add(textField);
     }
 }
